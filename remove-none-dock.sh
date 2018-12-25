6f713236e49115cf02f078b637a0ea94fc7d419e
Narjesse <narjess.derouich@gmail.com>
2018-12-25 22:07:57 +0100

    doc commit

--- remove-none-dock.sh
+++ remove-none-dock.sh
@@ -1,4 +1,4 @@
-tee= $(docker images | grep "^<none>" |  awk '{print $3}')
+tee= $(docker images | grep "<none>" |  awk '{print $3}')
 if [ ! -z $tee  ] ; then  
-docker rmi -f $(docker images | grep "^<none>" |  awk '{print $3}'); 
+docker rmi -f $(docker images | grep "<none>" |  awk '{print $3}'); 
 fi
