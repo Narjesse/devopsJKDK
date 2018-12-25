4897a304b569065da0c7ba404d0f5e3d1b1fcca9
Narjesse <narjess.derouich@gmail.com>
2018-12-25 11:47:28 +0100

    doc commit

--- save-docker.sh
+++ save-docker.sh
@@ -1,4 +1,5 @@
 docker ps
 #display the last created container (-n 1) quietly (q), will give us the container id
 id=`docker ps -n 1 -aq`
+echo "here is the id: $id"
 docker commit $id getintodevops/hellonode
