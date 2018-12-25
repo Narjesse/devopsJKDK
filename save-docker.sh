a64ef459c821376acdc2000db6fa436406cacbc7
Narjesse <narjess.derouich@gmail.com>
2018-12-25 11:36:59 +0100

    doc commit

--- save-docker.sh
+++ save-docker.sh
@@ -1,3 +1,4 @@
 docker ps
-id=`docker ps -aqf "name=fervent_lumiere"`
+#display the last created container (-n 1) quietly (q), will give us the container id
+id=`docker ps -n 1 -aq`
 docker commit $id getintodevops/hellonode
