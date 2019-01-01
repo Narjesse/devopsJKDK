ae65fe5f05bac751916e30a20464eded3d028e51
Narjesse <narjess.derouich@gmail.com>
2019-01-01 12:42:14 +0100

    ans part

--- save-docker.sh
+++ save-docker.sh
@@ -2,4 +2,4 @@ docker ps
 #display the last created container (-n 1) quietly (q), will give us the container id
 id=`docker ps -n 1 -aq`
 echo "here is the id: $id"
-docker commit $id getintodevops/hellonode
+docker commit $id narjess6/built-from-jenkins:latest
